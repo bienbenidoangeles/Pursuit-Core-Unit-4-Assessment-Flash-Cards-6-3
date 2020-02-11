6dcc038d6f7e3452e56b25e993b9a718b6ac5baf
Bienbenido Angeles <bienbenido.angeles@gmail.com>
2020-02-11 15:16:55 -0500

    added searchFlashCardsView

--- Unit4Assessment/Unit4Assessment/Controllers/SearchFlashCardsViewController.swift
+++ Unit4Assessment/Unit4Assessment/Controllers/SearchFlashCardsViewController.swift
@@ -12,6 +12,12 @@ import DataPersistence
 class SearchFlashCardsViewController: UIViewController {
     
     var dataPersistence: DataPersistence<FlashCard>!
+    
+    let searchFlashCardsView = SearchFlashCardsView()
+    
+    override func loadView() {
+        view = searchFlashCardsView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,15 +25,5 @@ class SearchFlashCardsViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
---
+++ Unit4Assessment/Unit4Assessment/Views/SearchFlashCardsView.swift
@@ -0,0 +1,46 @@
+//
+//  SearchFlashCardsView.swift
+//  Unit4Assessment
+//
+//  Created by Bienbenido Angeles on 2/11/20.
+//  Copyright © 2020 Alex Paul. All rights reserved.
+//
+
+import UIKit
+
+class SearchFlashCardsView: UIView {
+    
+    lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        let collectionView = UICollectionView(frame: UIScreen.main.bounds, collectionViewLayout: layout)
+        collectionView.backgroundColor = .systemGroupedBackground
+        return collectionView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: UIScreen.main.bounds)
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    private func commonInit(){
+        setupCollectionViewConstrainsts()
+    }
+    
+    private func setupCollectionViewConstrainsts(){
+        addSubview(collectionView)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+
+}
